def656d8f56286a31ad60aa95403619846939d3d
SIDDHESH-1403 <157562004+sidd-1403@users.noreply.github.com>
2026-04-05 22:58:13 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    //Dutch Nattinal Flag DNF
+    public void sortColors(int[] nums) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                low++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
+        }
+    }
+}
