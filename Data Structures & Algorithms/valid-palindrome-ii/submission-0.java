38d88d59bc38200bd760b585a3cafa6378297c31
SIDDHESH-1403 <157562004+sidd-1403@users.noreply.github.com>
2026-04-23 13:50:20 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return check(i + 1, j, s) || check(i, j - 1, s);
+            }
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+
+    private boolean check(int i, int j, String s) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
