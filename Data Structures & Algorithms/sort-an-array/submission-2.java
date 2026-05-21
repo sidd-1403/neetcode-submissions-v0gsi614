a480124d060b3e13d29bb56018d7baa092d15140
SIDDHESH-1403 <157562004+sidd-1403@users.noreply.github.com>
2026-05-21 23:53:26 +0530

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n=nums.length;
+        mergeSort(nums,0,n-1);
+        return nums;
+    }
+
+    public void mergeSort(int[] nums, int start,int end)
+    {
+        if(start==end)
+        {
+            return;
+        }
+
+        int mid=(start+end)/2;
+        mergeSort(nums,start,mid);
+        mergeSort(nums,mid+1,end);
+
+        merge(nums,start,mid,end);
+    }
+    public void merge(int[] nums,int start,int mid,int end)
+    {
+        int i=start;
+        int j=mid+1;
+
+        ArrayList<Integer>list=new ArrayList<>();
+
+        while(i<=mid && j<=end)
+        {
+            if(nums[i]<nums[j])
+            {
+                list.add(nums[i]);
+                i++;
+            }
+            else
+            {
+                list.add(nums[j]);
+                j++;
+            }
+        }
+
+        while(i<=mid)
+        {
+            list.add(nums[i]);
+            i++; 
+        }
+
+        while(j<=end)
+        {
+            list.add(nums[j]);
+            j++; 
+        }
+
+        int index=0;
+        for(int k=start;k<=end;k++)
+        {
+            nums[k]=list.get(index);
+            index++;
+        }
+
+    }
+}
